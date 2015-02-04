a7ec81e30b95c4d757bf724069ef3567bffd2f45
Joshua Sperry <Dinner1111@users.noreply.github.com>
2015-02-03 20:52:34 -0500

    Add alphabet randomizer

---
+++ Alphabet/Program.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<String, String> alphaEquiv = new Dictionary<String, String>();
+            List<String> alphabet = new List<String> {
+                "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", 
+                "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z" 
+            };
+            Random random = new Random();
+
+            foreach (string letter in alphabet.ToArray())
+            {
+                string value = alphabet[random.Next(alphabet.Count - 1)];
+
+                alphabet.Remove(value);
+                alphaEquiv.Add(letter, value);
+            }
+
+            int index = 0;
+            string[] equivs = new string[alphaEquiv.Count];
+            foreach (string key in alphaEquiv.Keys)
+            {
+                equivs[index] = key + " = " + alphaEquiv[key];
+                index++;
+            }
+
+            System.IO.File.WriteAllLines(@"C:\Users\mrpep_000\Desktop\alphabet.txt", 
+                equivs);
+        }
+    }
+}
